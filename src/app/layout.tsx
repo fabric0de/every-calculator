348e789237b856c3dbf6a5abcb56ee161982b368
fabric0de <solee3013@gmail.com>
2024-07-14 22:14:37 +0900

    feat: 사용자 지표 추적을 위한 구글 애널리틱스 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Footer from "@/components/Footer";
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,8 +9,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
   return (
     <html lang="ko">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `}
+        </Script>
+      </head>
       <body>
         <main>{children}</main>
         <Footer />
