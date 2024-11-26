fac0bebc253a6d03a8e50691b0e164d13384717e
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2024-11-26 17:37:11 +0900

    fix: Google analytics 주석 (임시 비활성)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,23 @@
 import Footer from "@/components/Footer";
 import Script from "next/script";
 import "./globals.css";
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+  // const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
   return (
     <html lang="ko">
       <head>
-        <Script
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-status-bar-style" content="black" />
+        <meta name="mobile-web-app-title" content="Every Calculator" />
+        {/* <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           strategy="afterInteractive"
         />
@@ -24,7 +30,7 @@ export default function RootLayout({
             gtag('js', new Date());
             gtag('config', '${GA_TRACKING_ID}');
           `}
-        </Script>
+        </Script> */}
       </head>
       <body>
         <main>{children}</main>
